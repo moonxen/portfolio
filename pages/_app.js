5f6818c9ac374615ef8d2e49eb53a73694e892c3
oguzhanbalta <oguzhanbalta99@gmail.com>
2022-04-14 11:57:39 +0300

    theme, mui added.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import React, {useEffect, useState} from 'react';
+import { MuiThemeProvider, useMediaQuery, CssBaseline } from '@material-ui/core';
+import {darkTheme, lightTheme} from "../themes";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+    const [theme, setTheme] = useState(
+        prefersDarkMode ? darkTheme : lightTheme
+    )
+
+    useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </MuiThemeProvider>
+
+
+  )
 }
 
 export default MyApp
---
+++ themes/index.js
@@ -0,0 +1,16 @@
+import { createMuiTheme } from "@material-ui/core";
+
+
+export const lightTheme = createMuiTheme({
+    type: 'light',
+    theme: {
+        colors: {}, // optional
+    }
+})
+
+export const darkTheme = createMuiTheme({
+    type: 'dark',
+    theme: {
+        colors: {}, // optional
+    }
+})
